857f1a2da8fe83fca19aa1922f9d487df2669c49
Andrei Belokopytov <belokopytov@live.com>
2020-03-09 14:57:13 +0300

    refactoring

---
+++ lib/crypto_list_item.dart
@@ -0,0 +1,80 @@
+import 'dart:math';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'data/crypto_data.dart';
+
+class CryptoListItem extends StatelessWidget {
+  static const imageSize = 48.0;
+  static const colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.amber,
+    Colors.purple
+  ];
+
+  final Crypto currency;
+
+  CryptoListItem(this.currency);
+
+  @override
+  Widget build(BuildContext context) {
+    final placeholder = ClipOval(
+      child: Container(
+        color: Color.fromRGBO(242, 242, 242, 1),
+        width: imageSize,
+        height: imageSize,
+      ),
+    );
+
+    final errorImage = ClipOval(
+      child: Container(
+        width: imageSize,
+        height: imageSize,
+        color: colors[Random().nextInt(colors.length - 1)],
+        child: Center(
+          child: Text(
+            currency.name[0].toUpperCase(),
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+
+    return ListTile(
+      leading: CachedNetworkImage(
+        width: imageSize,
+        height: imageSize,
+        imageUrl:
+            "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9867bdb19da14e63ffbe63805298fa60bf255cdd/32@2x/color/${currency.symbol?.toLowerCase()}@2x.png",
+        placeholder: (context, url) => placeholder,
+        errorWidget: (context, url, error) => errorImage,
+      ),
+      title: Text(currency.name, style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: _getSubtitleText(currency.price, currency.percentChange),
+      isThreeLine: true,
+    );
+  }
+
+  Widget _getSubtitleText(double priceUSD, double percentageChange) {
+    var priceTextWidget = TextSpan(
+        text: "\$${priceUSD.toStringAsFixed(2)}\n",
+        style: TextStyle(color: Colors.black));
+    var percentageChangeText =
+        "1 hour: ${percentageChange.toStringAsFixed(2)}%";
+    TextSpan percentageChangeTextWidget;
+
+    if (percentageChange > 0) {
+      percentageChangeTextWidget = TextSpan(
+          text: percentageChangeText, style: TextStyle(color: Colors.green));
+    } else {
+      percentageChangeTextWidget = TextSpan(
+          text: percentageChangeText, style: TextStyle(color: Colors.red));
+    }
+
+    return RichText(
+        text:
+            TextSpan(children: [priceTextWidget, percentageChangeTextWidget]));
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,7 @@
-import 'dart:math';
-
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'bloc/currencies_block.dart';
+import 'crypto_list_item.dart';
 import 'data/crypto_data.dart';
 import 'providers/bloc_provider.dart';
 
@@ -40,8 +38,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _getRowWithDivider(Crypto currency) {
-    var children = <Widget>[
-      Padding(padding: EdgeInsets.all(10.0), child: _getListItemUi(currency)),
+    final children = <Widget>[
+      Padding(padding: EdgeInsets.all(10.0), child: CryptoListItem(currency)),
       Divider(height: 5.0),
     ];
 
@@ -49,77 +47,5 @@ class HomePage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: children,
     );
-  }
-
-  ListTile _getListItemUi(Crypto currency) {
-    const imageSize = 48.0;
-
-    final colors = [
-      Colors.blue,
-      Colors.red,
-      Colors.green,
-      Colors.amber,
-      Colors.purple
-    ];
-
-    final placeholder = ClipOval(
-      child: Container(
-        color: Color.fromRGBO(242, 242, 242, 1),
-        width: imageSize,
-        height: imageSize,
-      ),
-    );
-
-    final errorImage = ClipOval(
-      child: Container(
-        width: imageSize,
-        height: imageSize,
-        color: colors[Random().nextInt(colors.length - 1)],
-        child: Center(
-          child: Text(
-            currency.name[0].toUpperCase(),
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.white
-            ),
-          ),
-        ),
-      ),
-    );
-
-    return ListTile(
-      leading: CachedNetworkImage(
-        width: imageSize,
-        height: imageSize,
-        imageUrl:
-            "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9867bdb19da14e63ffbe63805298fa60bf255cdd/32@2x/color/${currency.symbol?.toLowerCase()}@2x.png",
-        placeholder: (context, url) => placeholder,
-        errorWidget: (context, url, error) => errorImage,
-      ),
-      title: Text(currency.name, style: TextStyle(fontWeight: FontWeight.bold)),
-      subtitle: _getSubtitleText(currency.price, currency.percentChange),
-      isThreeLine: true,
-    );
-  }
-
-  Widget _getSubtitleText(double priceUSD, double percentageChange) {
-    var priceTextWidget = TextSpan(
-        text: "\$${priceUSD.toStringAsFixed(2)}\n",
-        style: TextStyle(color: Colors.black));
-    var percentageChangeText =
-        "1 hour: ${percentageChange.toStringAsFixed(2)}%";
-    TextSpan percentageChangeTextWidget;
-
-    if (percentageChange > 0) {
-      percentageChangeTextWidget = TextSpan(
-          text: percentageChangeText, style: TextStyle(color: Colors.green));
-    } else {
-      percentageChangeTextWidget = TextSpan(
-          text: percentageChangeText, style: TextStyle(color: Colors.red));
-    }
-
-    return RichText(
-        text:
-            TextSpan(children: [priceTextWidget, percentageChangeTextWidget]));
   }
 }
